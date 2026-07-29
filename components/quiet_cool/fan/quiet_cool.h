510f3585ec30eb47e2805ff706bde3cb80291f5c
macmacattack <130704322+macmacattack@users.noreply.github.com>
2026-07-28 21:13:35 -0700

    Update quiet_cool.h

--- components/quiet_cool/fan/quiet_cool.h
+++ components/quiet_cool/fan/quiet_cool.h
@@ -12,14 +12,68 @@ class QuietCoolFan : public Component, public fan::Fan, public spi::SPIDevice<sp
  public:
   QuietCoolFan() = default;
 
-  void set_pins(uint8_t csn, uint8_t gdo0, uint8_t gdo2);
-  void set_remote_id(const std::vector<uint8_t> &remote_id);
-  void set_frequencies(float center_freq, float deviation);
+  void set_pins(uint8_t csn, uint8_t gdo0, uint8_t gdo2) {
+    this->csn_pin_ = csn;
+    this->gdo0_pin_ = gdo0;
+    this->gdo2_pin_ = gdo2;
+  }
 
-  void setup() override;
-  fan::FanTraits get_traits() override;
-  void control(const fan::FanCall &call) override;
-  void dump_config() override;
+  void set_remote_id(const std::vector<uint8_t> &remote_id) {
+    for (size_t i = 0; i < 7 && i < remote_id.size(); i++) {
+      this->remote_id_[i] = remote_id[i];
+    }
+  }
+
+  void set_frequencies(float center_freq, float deviation) {
+    this->center_freq_mhz_ = center_freq;
+    this->deviation_khz_ = deviation;
+  }
+
+  void setup() override {
+    // Instantiate QuietCool using ESP32-S3 Hardware SPI pins: SCK=12, MISO=13, MOSI=11
+    this->qc_ = new QuietCool(this->csn_pin_, this->gdo0_pin_, this->gdo2_pin_, 12, 13, 11, this->remote_id_, this->center_freq_mhz_, this->deviation_khz_);
+    this->qc_->begin();
+  }
+
+  fan::FanTraits get_traits() override {
+    auto traits = fan::FanTraits();
+    traits.set_speed(true);
+    return traits;
+  }
+
+  void control(const fan::FanCall &call) override {
+    if (call.get_state().has_value()) {
+      bool state = *call.get_state();
+      if (!state) {
+        this->qc_->send(QUIETCOOL_SPEED_HIGH, QUIETCOOL_DURATION_OFF);
+        this->state = false;
+      } else {
+        this->state = true;
+      }
+    }
+
+    if (call.get_speed().has_value()) {
+      int speed = *call.get_speed();
+      if (this->state) {
+        if (speed == 1) {
+          this->qc_->send(QUIETCOOL_SPEED_LOW, QUIETCOOL_DURATION_ON);
+        } else if (speed == 2) {
+          this->qc_->send(QUIETCOOL_SPEED_MEDIUM, QUIETCOOL_DURATION_ON);
+        } else {
+          this->qc_->send(QUIETCOOL_SPEED_HIGH, QUIETCOOL_DURATION_ON);
+        }
+      }
+      this->speed = speed;
+    }
+    this->publish_state();
+  }
+
+  void dump_config() override {
+    ESP_LOGCONFIG("quiet_cool.fan", "QuietCool Fan:");
+    ESP_LOGCONFIG("quiet_cool.fan", "  CS Pin: %d", this->csn_pin_);
+    ESP_LOGCONFIG("quiet_cool.fan", "  GDO0 Pin: %d", this->gdo0_pin_);
+    ESP_LOGCONFIG("quiet_cool.fan", "  GDO2 Pin: %d", this->gdo2_pin_);
+  }
 
  protected:
   uint8_t csn_pin_{10};
