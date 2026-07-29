4ec1520da6fbffee81d17013166fc5e445ec749f
macmacattack <130704322+macmacattack@users.noreply.github.com>
2026-07-28 21:02:50 -0700

    Update quiet_cool.h

--- components/quiet_cool/fan/quiet_cool.h
+++ components/quiet_cool/fan/quiet_cool.h
@@ -1,55 +1,83 @@
 #pragma once
 
-#include "esphome/components/fan/fan.h"
 #include "esphome/core/component.h"
+#include "esphome/components/fan/fan.h"
 #include "esphome/components/spi/spi.h"
 #include "quietcool.h"
-#include <memory>
 
 namespace esphome {
-    namespace quiet_cool {
+namespace quiet_cool {
 
-        class QuietCoolFan :
-	    public Component,
-	    public fan::Fan,
-	    public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1KHZ>
-	{
-        public:
+class QuietCoolFan : public Component, public fan::Fan, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_2MHZ> {
+ public:
+  QuietCoolFan() = default;
 
-            void dump_config() override;
-            fan::FanTraits get_traits() override;
-            void setup() override;  // initialise radio
-            float get_setup_priority() const override { return setup_priority::BUS; }
-            void set_pins(uint8_t csn, uint8_t gdo0, uint8_t gdo2) {
-                this->csn_pin_ = csn;
-                this->gdo0_pin_ = gdo0;
-                this->gdo2_pin_ = gdo2;
-                this->pins_set_ = true;
-            }
-	    void set_frequencies(float center_freq_mhz, float devation_khz) {
-		this->center_freq_mhz = center_freq_mhz;
-		this->deviation_khz = deviation_khz;
-	    }
+  void set_pins(uint8_t csn, uint8_t gdo0, uint8_t gdo2) {
+    this->csn_pin_ = csn;
+    this->gdo0_pin_ = gdo0;
+    this->gdo2_pin_ = gdo2;
+  }
 
-        protected:
-            void control(const fan::FanCall &call) override;
-            void write_state_();
-        private:
-            std::unique_ptr<QuietCool> qc_;
+  void set_remote_id(const std::vector<uint8_t> &remote_id) {
+    for (size_t i = 0; i < 7 && i < remote_id.size(); i++) {
+      this->remote_id_[i] = remote_id[i];
+    }
+  }
 
-            uint8_t csn_pin_{};
-            uint8_t gdo0_pin_{};
-            uint8_t gdo2_pin_{};
-	    float center_freq_mhz{433.897};
-	    float deviation_khz{10};
-            float speed_{0.0f};
-            bool pins_set_{false};
-            std::array<uint8_t, 7> remote_id_{{0x2D, 0xD4, 0x06, 0xCB, 0x00, 0xF7, 0xF2}};
-        public:
-            void set_remote_id(const std::vector<uint8_t> &remote_id) {
-                for (size_t i = 0; i < 7 && i < remote_id.size(); ++i) remote_id_[i] = remote_id[i];
-            }
-        };
+  void set_frequencies(float center_freq, float deviation) {
+    this->center_freq_mhz_ = center_freq;
+    this->deviation_khz_ = deviation;
+  }
 
-    }  // namespace quiet_cool
+  void setup() override {
+    // Instantiate QuietCool using ESP32-S3 Hardware SPI pins: SCK=12, MISO=13, MOSI=11
+    this->qc_ = new QuietCool(this->csn_pin_, this->gdo0_pin_, this->gdo2_pin_, 12, 13, 11, this->remote_id_, this->center_freq_mhz_, this->deviation_khz_);
+    this->qc_->begin();
+  }
+
+  fan::FanTraits get_traits() override {
+    auto traits = fan::FanTraits();
+    traits.set_speed(true);
+    traits.set_supported_preset_modes({});
+    return traits;
+  }
+
+  void control(const fan::FanCall &call) override {
+    if (call.get_state().has_value()) {
+      bool state = *call.get_state();
+      if (!state) {
+        this->qc_->send(QUIETCOOL_SPEED_HIGH, QUIETCOOL_DURATION_OFF);
+        this->state = false;
+      } else {
+        this->state = true;
+      }
+    }
+
+    if (call.get_speed().has_value()) {
+      int speed = *call.get_speed();
+      if (this->state) {
+        if (speed == 1) {
+          this->qc_->send(QUIETCOOL_SPEED_LOW, QUIETCOOL_DURATION_ON);
+        } else if (speed == 2) {
+          this->qc_->send(QUIETCOOL_SPEED_MEDIUM, QUIETCOOL_DURATION_ON);
+        } else {
+          this->qc_->send(QUIETCOOL_SPEED_HIGH, QUIETCOOL_DURATION_ON);
+        }
+      }
+      this->speed = speed;
+    }
+    this->publish_state();
+  }
+
+ protected:
+  uint8_t csn_pin_{10};
+  uint8_t gdo0_pin_{14};
+  uint8_t gdo2_pin_{15};
+  uint8_t remote_id_[7]{0};
+  float center_freq_mhz_{433.897f};
+  float deviation_khz_{10.0f};
+  QuietCool *qc_{nullptr};
+};
+
+}  // namespace quiet_cool
 }  // namespace esphome
